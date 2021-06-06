d3daa140f53ca953062a1f313298c8df80b210fd
paulcode83 <p.codega@gmail.com>
2021-06-06 21:31:53 +0200

    fix bug

--- PatternVisitor/Prova.cs
+++ PatternVisitor/Prova.cs
@@ -6,10 +6,12 @@ public class Prova
 
    public int MyProperty4 { get; set; }
    public int MyProperty5 { get; set; }
-  
+
+   public int MyProperty6 { get; set; }
+   public int MyProperty7 { get; set; }
 
 
-   
+
 
    public Prova()
    {
