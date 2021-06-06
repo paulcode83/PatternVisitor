ab121c3f663a7c059ff28e3bdf9b446b479cee78
paulcode83 <p.codega@gmail.com>
2021-06-06 16:28:15 +0200

    aggiunte props

--- PatternVisitor/Prova.cs
+++ PatternVisitor/Prova.cs
@@ -3,9 +3,15 @@ public class Prova
    public int MyProperty { get; set; }
    public int MyProperty2 { get; set; }
    public int MyProperty3 { get; set; }
+
+   public int MyProperty4 { get; set; }
+   public int MyProperty5 { get; set; }
+   public int MyProperty6 { get; set; }
+
+
    public Prova()
    {
-      public int MyProperty { get; set; }
-}
+
+   }
 }
 
