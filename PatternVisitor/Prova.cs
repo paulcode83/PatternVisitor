ed62ca010d869ede2d921db5bbe9d6dc265337cf
paulcode83 <p.codega@gmail.com>
2021-06-06 16:35:49 +0200

    Revert "Revert "2""
    This reverts commit 1a76b25614adeb11fb8161c874b4313f719edbd2.

--- PatternVisitor/Prova.cs
+++ PatternVisitor/Prova.cs
@@ -9,8 +9,7 @@ public class Prova
    public int MyProperty6 { get; set; }
 
    public int MyProperty7 { get; set; }
-   public int MyProperty8 { get; set; }
-   public int MyProperty9 { get; set; }
+   
 
    public Prova()
    {
